1354157cca970c59b17f5dfc30bd5b86eed861ab
Gagan-Saini-GS <gagansaini10999@gmail.com>
2025-08-01 20:31:12 +0530

    Changed api index.js and verceljson for deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
-import serverless from "serverless-http";
+import serverlessHttp from "serverless-http";
+import dotenv from "dotenv";
+import connectDB from "../src/db/index.js";
 import { app } from "../src/app.js";
 
-export const handler = serverless(app);
+dotenv.config();
+
+let isConnected;
+
+async function bootstrap() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app;
+}
+
+export const handler = async (event, context) => {
+  const appInstance = await bootstrap();
+  const handlerFn = serverlessHttp(appInstance);
+  return handlerFn(event, context);
+};
