5accb249676408785a3550cbd0eb38d3da5e6ef5
Gagan-Saini-GS <gagansaini10999@gmail.com>
2025-08-01 20:25:40 +0530

    Deployment server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import { app } from "./app.js";
+// This is for local only, vercel will not run this.
 
-dotenv.config();
+import { app } from "./app.js";
 
 const port = process.env.PORT || 8000;
 
-connectDB()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`✅ Server is running at http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log("DB connection failed !!! ", err);
-  });
+app.listen(port, () => {
+  console.log(`✅ Local server running at http://localhost:${port}`);
+});
+
+// import dotenv from "dotenv";
+// import connectDB from "./db/index.js";
+// import { app } from "./app.js";
+
+// dotenv.config();
+
+// const port = process.env.PORT || 8000;
+
+// connectDB()
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`✅ Server is running at http://localhost:${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("DB connection failed !!! ", err);
+//   });
