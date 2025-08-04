b8267b5c70b0f5e57830fe2fb3bcf0087f0366ad
Gagan-Saini-GS <gagansaini10999@gmail.com>
2025-08-04 19:15:12 +0530

    Starting deployment from scratch

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Chal ja bhai mere");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+export default app;
+
 // import serverlessHttp from "serverless-http";
-// import dotenv from "dotenv";
 // import connectDB from "../src/db/index.js";
 // import { app } from "../src/app.js";
-
-// dotenv.config();
 
 // let isConnected;
 
@@ -15,37 +25,17 @@
 //   return app;
 // }
 
-// export const handler = async (event, context) => {
+// const handler = async (event, context) => {
+//   console.log("🚀 Handler called with event:", {
+//     path: event.path,
+//     method: event.httpMethod,
+//     headers: event.headers,
+//   });
+
 //   const appInstance = await bootstrap();
 //   const handlerFn = serverlessHttp(appInstance);
 //   return handlerFn(event, context);
 // };
 
-import serverlessHttp from "serverless-http";
-import connectDB from "../src/db/index.js";
-import { app } from "../src/app.js";
-
-let isConnected;
-
-async function bootstrap() {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  return app;
-}
-
-const handler = async (event, context) => {
-  console.log("🚀 Handler called with event:", {
-    path: event.path,
-    method: event.httpMethod,
-    headers: event.headers,
-  });
-
-  const appInstance = await bootstrap();
-  const handlerFn = serverlessHttp(appInstance);
-  return handlerFn(event, context);
-};
-
-// Export for Vercel
-export default handler;
+// // Export for Vercel
+// export default handler;
